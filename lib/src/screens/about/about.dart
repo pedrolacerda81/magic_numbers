473afbfb5756b9276037d077d2f7dff4e9098991
Pedro - GitPro <peidom81@gmail.com>
2020-02-03 11:46:11 -0400

    Adds gitHub button on about screen

--- lib/src/screens/about/about.dart
+++ lib/src/screens/about/about.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class About extends StatelessWidget {
+  void _launchGitHubUrl() async {
+    const url = 'https://github.com/pedrolacerda81';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Não consegui acessar $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,8 +38,8 @@ class About extends StatelessWidget {
                   alignment: Alignment.center,
                   children: <Widget>[
                     Container(
-                      height: 120.0,
-                      width: 120.0,
+                      height: 125.0,
+                      width: 125.0,
                       decoration: BoxDecoration(
                         shape: BoxShape.circle,
                         gradient: LinearGradient(
@@ -43,8 +53,8 @@ class About extends StatelessWidget {
                       ),
                     ),
                     Container(
-                      height: 110.0,
-                      width: 110.0,
+                      height: 120.0,
+                      width: 120.0,
                       decoration: BoxDecoration(
                         shape: BoxShape.circle,
                         image: DecorationImage(
@@ -54,6 +64,45 @@ class About extends StatelessWidget {
                       ),
                     ),
                   ],
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  'PEDRO LACERDA DA COSTA',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                SizedBox(height: 3.0),
+                Text(
+                  'Flutter Developer',
+                  style: TextStyle(fontSize: 14.0, color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Text('hello.'),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  splashColor: Colors.white,
+                  color: Colors.deepPurpleAccent,
+                  child: Text(
+                    'GITHUB',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  onPressed: () {
+                    print("pressed 'go to github' button!");
+                    _launchGitHubUrl();
+                  },
                 ),
               ],
             ),
